c6608a750b65e7af49c63f23753998798a71729e
prachiti98 <prachitiparkar98@gmail.com>
2019-04-15 19:44:13 +0530

    Second commit

--- app.js
+++ app.js
@@ -82,6 +82,45 @@ app.post('/articles/add',function(req,res){
 	});
 });
 
+app.get('/article/edit/:id',function(req,res){
+	Article.findById(req.params.id,function(err,article){
+		res.render('edit_article',{
+			title:'Edit Article',
+			article:article
+		});
+	});
+});
+
+app.post('/articles/edit/:id',function(req,res){
+	let article = {};
+	article.title = req.body.title;
+	article.author = req.body.author;
+	article.body = req.body.body;
+
+	let query = {_id:req.params.id}
+
+	Article.update(query,article,function(err){
+		if(err){
+			console.log(err);
+			return;
+		}
+		else{
+			res.redirect('/');
+		}
+	});
+});
+
+
+app.delete('article/:id',function(req,res){
+	let query = {_id:req.params.id}
+
+	Article.remove(query,function(err){
+		if(err){
+			consolelog(err);
+		}
+		res.send('Success');
+	});
+});
 
 app.listen(3000,function(){
 	console.log("server started on port 3000");
